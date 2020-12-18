c7be5c293d2bc0c10c02d10e93eeba8f49689a64
Isaac Quintanilla <isaac@iqbiostats.com>
2020-12-18 15:08:22 -0800

    Minor Changes

--- presentations/Cluster_Script.sh
+++ presentations/Cluster_Script.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1 # Setting the number of tasks, usually 1 task is used
 #SBATCH --cpus-per-task=8 # Setting the number of cpus per task, this is the number of cores you specify
 #SBATCH --mem-per-cpu=1G # How much ram per cpu, usually 1 GB
-#SBATCH --time=01:00:00    # Time to run task, changes based on predicted time of task, set to 1 hour
+#SBATCH --time=0-00:15:00    # Time to run task, changes based on predicted time of task, set to 1 hour
 #SBATCH --output=my.stdout # Where to store the output, usually a standard output, I don't use it
 #SBATCH --mail-user=NETID@ucr.edu # Where to email information about job
 #SBATCH --mail-type=ALL # Not Sure
